9f995c23ccf9b8bab1479d729fa64b3af64f3f7e
Archie White <erniewhite@embarqmail.com>
2018-08-29 06:16:18 -0500

    Added in borrowed code from the Click library

--- tomlconf.py
+++ tomlconf.py
@@ -1,3 +1,75 @@
+import os
+import sys
+
+
+"""This section of code taken from the Click module instead of bring in the entire
+library for this one function.
+Please visit http://click.pocoo.org for more information on the click library
+
+The comment END CLICK CODE below marks the end of the click library code 
+"""
+WIN = sys.platform.startswith('win')
+
+
+def _posixify(name):
+    return '-'.join(name.split()).lower()
+
+
+def get_app_dir(app_name, roaming=True, force_posix=False):
+    r"""Returns the config folder for the application.  The default behavior
+    is to return whatever is most appropriate for the operating system.
+
+    To give you an idea, for an app called ``"Foo Bar"``, something like
+    the following folders could be returned:
+
+    Mac OS X:
+      ``~/Library/Application Support/Foo Bar``
+    Mac OS X (POSIX):
+      ``~/.foo-bar``
+    Unix:
+      ``~/.config/foo-bar``
+    Unix (POSIX):
+      ``~/.foo-bar``
+    Win XP (roaming):
+      ``C:\Documents and Settings\<user>\Local Settings\Application Data\Foo Bar``
+    Win XP (not roaming):
+      ``C:\Documents and Settings\<user>\Application Data\Foo Bar``
+    Win 7 (roaming):
+      ``C:\Users\<user>\AppData\Roaming\Foo Bar``
+    Win 7 (not roaming):
+      ``C:\Users\<user>\AppData\Local\Foo Bar``
+
+    .. versionadded:: 2.0
+
+    :param app_name: the application name.  This should be properly capitalized
+                     and can contain whitespace.
+    :param roaming: controls if the folder should be roaming or not on Windows.
+                    Has no affect otherwise.
+    :param force_posix: if this is set to `True` then on any POSIX system the
+                        folder will be stored in the home folder with a leading
+                        dot instead of the XDG config home or darwin's
+                        application support folder.
+    """
+    if WIN:
+        key = roaming and 'APPDATA' or 'LOCALAPPDATA'
+        folder = os.environ.get(key)
+        if folder is None:
+            folder = os.path.expanduser('~')
+        return os.path.join(folder, app_name)
+    if force_posix:
+        return os.path.join(os.path.expanduser('~/.' + _posixify(app_name)))
+    if sys.platform == 'darwin':
+        return os.path.join(os.path.expanduser(
+            '~/Library/Application Support'), app_name)
+    return os.path.join(
+        os.environ.get('XDG_CONFIG_HOME', os.path.expanduser('~/.config')),
+        _posixify(app_name))
+
+
+"""***********************************************
+                   END CLICK CODE
+***********************************************"""
+
 class File:
     """File context manager
 
@@ -50,3 +122,4 @@ class File:
                 self.__openfile.write(str(self.text))
                 self.__openfile.truncate()
             self.__openfile.close()
+
