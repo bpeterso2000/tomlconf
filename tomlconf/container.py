4b257a7a53bed86c220ab81673bc42fabf2a8b13
Brian Peterson <bpeterso2000@yahoo.com>
2018-11-29 19:43:47 -0500

    Include TOMLDocument with excecption handling

---
+++ tomlconf/container.py
@@ -0,0 +1,31 @@
+import tomlkit
+
+from .errors import KeyAlreadyPresent, NonExistentKey
+
+
+class TOMLDocument(tomlkit.container.Container):
+    """A TOML document."""
+
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except tomlkit.exceptions.NonExistentKey as e:
+            raise NonExistentKey(e)
+
+    def append(self, key, item):
+        try:
+            super().append(key, item)
+        except tomlkit.exceptions.KeyAlreadyPresent as e:
+            raise KeyAlreadyPresent(e)
+
+    def remove(self, key):
+        try:
+            super().remove(key)
+        except tomlkit.exceptions.NonExistentKey as e:
+            raise NonExistentKey(e)
+
+    def item(self, key):
+        try:
+            return super().item(key)
+        except tomlkit.exceptions.NonExistentKey as e:
+            raise NonExistentKey(e)
