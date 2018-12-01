4e4405f9c93811ae525619bb5dfe8c393ea620f8
Archie White <erniewhite@embarqmail.com>
2018-11-30 18:32:55 -0600

    dummy

--- tests/test_tomlconf.py
+++ tests/test_tomlconf.py
@@ -1,8 +1,11 @@
-import pytest
 import os
-import tomlkit
 
-from tomlconf import Config, WIN, MAC, get_app_dir
+import pytest
+import tomlkit
+import tomlkit.exceptions
+
+import tomlconf
+from tomlconf import Config, WIN, MAC, get_app_dir, TomlConfError, ModeError
 
 file_content = """# This is a TOML document.
 
@@ -11,7 +14,7 @@ title = "TOML Example"
 [owner]
 name = "Tom Preston-Werner"
 organization = "GitHub"
-bio = "GitHub Cofounder & CEO\nLikes tater tots and beer."
+bio = "GitHub Cofounder & CEO Likes tater tots and beer."
 dob = 1979-05-27T07:32:00Z # First class dates? Why not?
 
 [database]
@@ -19,6 +22,33 @@ server = "192.168.1.1"
 ports = [8001, 8001, 8002]
 connection_max = 5000
 enabled = true
+"""
+
+bad_file_content = """# This is a TOML document.
+
+title = "TOML Example"
+
+[owner]
+name = "Tom Preston-Werner"
+name = "Tom Preston-Werner"
+organization = "GitHub"
+bio = "GitHub Cofounder & CEO Likes tater tots and beer."
+dob = 1979-05-27T07:32:00Z # First class dates? Why not?
+
+[database]
+server = "192.168.1.1"
+ports = [8001, 8001, 8002]
+connection_max = 5000
+enabled = true
+"""
+
+DOUBLE_KEY = """# TOML with double key error
+
+title = "TOML Excample"
+
+[owner]
+name = "Archie"
+name = "Ernest"
 """
 
 old_data = tomlkit.parse(file_content)
@@ -31,8 +61,17 @@ foo = "bar"
 [table2]
 array = [1, 2, 3]
 """
-new_data = tomlkit.parse(file_content_2)
 
+file_content_3 = """
+[table]
+baz = 13
+foo = "bar"
+
+[table2]
+array = [1, 2, 3]
+"""
+
+new_data = tomlkit.parse(file_content_2)
 
 toml_doc = tomlkit.loads(file_content)
 
@@ -44,6 +83,15 @@ def tmpfile(tmpdir):
     p = tmpdir.mkdir('testdir').join('testfile.txt')
     p.write(tomlkit.dumps(toml_doc))
     return str(p)
+
+
+@pytest.fixture
+def tmpfile_bad(tmpdir):
+    def _foo(data_string):
+        p = tmpdir.mkdir('testdir').join('testfile.txt')
+        p.write(data_string)
+        return str(p)
+    return _foo
 
 
 def test_read_only(tmpfile):
@@ -72,9 +120,17 @@ def test_read_write(tmpfile):
 
 
 def test_invalid_mode(tmpfile):
-    with pytest.raises(ValueError):
+    with pytest.raises(ModeError):
         with Config(tmpfile, 'w+'):
             pass
+
+
+def test_tomlconferror(tmpfile):
+    try:
+        with Config(tmpfile, 'q'):
+            pass
+    except TomlConfError as e:
+        assert str(e) == 'File mode must be "r", "r+" or "w" not %s' % '"q"'
 
 
 def test_encoding(tmpfile):
@@ -140,3 +196,27 @@ def test_get_nix_app_dir():
     )
     result = get_app_dir('Foo Bar')
     assert app_dir in result and 'foo-bar' in result
+
+
+def test_TomlConfError(tmpfile_bad):
+    try:
+        with Config(tmpfile_bad(DOUBLE_KEY), 'r'):
+            pass
+    except tomlconf.TomlConfError as e:
+        assert str(e) == 'Key "name" already exists.'
+
+
+# def test_FileError(tmpfile_bad):
+#     try:
+#         with Config('filedoesnotexist.txt', 'r'):
+#             pass
+#     except tomlconf.FileError as e:
+#         assert str(e) == "[Errno 2] No such file or directory: ' '"
+
+
+def test_KeyAlreadyPresent(tmpfile_bad):
+    try:
+        with Config(tmpfile_bad(DOUBLE_KEY), 'r'):
+            pass
+    except tomlconf.KeyAlreadyPresent as e:
+        assert str(e) == 'Key "name" already exists.'
