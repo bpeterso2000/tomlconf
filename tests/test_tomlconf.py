ce0f489b196a562a788440aec98729fa9df79c4c
Archie White <erniewhite@embarqmail.com>
2018-12-12 21:01:24 -0600

    Somehow messed up my code base after doing a pull request totally lost my core.py file

--- tests/test_tomlconf.py
+++ tests/test_tomlconf.py
@@ -1,19 +1,18 @@
 import pytest
 import os
+import sys
 import tomlkit
 
-from tomlconf.core import Config, WIN, MAC, get_app_dir
+
+from tomlconf.core import Config, WIN, MAC, get_app_dir, get_filename, stem
 
 file_content = """# This is a TOML document.
-
 title = "TOML Example"
-
 [owner]
 name = "Tom Preston-Werner"
 organization = "GitHub"
 bio = "GitHub Cofounder & CEO\\nLikes tater tots and beer."
 dob = 1979-05-27T07:32:00Z # First class dates? Why not?
-
 [database]
 server = "192.168.1.1"
 ports = [8001, 8001, 8002]
@@ -27,7 +26,6 @@ file_content_2 = """
 [table]
 baz = 13
 foo = "bar"
-
 [table2]
 array = [1, 2, 3]
 """
@@ -36,10 +34,29 @@ new_data = tomlkit.parse(file_content_2)
 toml_doc = tomlkit.loads(file_content)
 toml_blank = tomlkit.document()
 
+get_filename_params = [
+    ('', True, False),
+    ('', False, True),
+    ('', True, True),
+    ('', False, False),
+    ('/Users/archiewhite', True, False),
+    ('/Users/archiewhite', False, True),
+    ('/Users/archiewhite', True, True),
+    ('/Users/archiewhite', False, False),
+    ('fubar', True, False),
+    ('fubar', False, True),
+    ('fubar', True, True),
+    ('fubar', False, False),
+    ('/hello/bob.toml', True, False),
+    ('/hello/bob.toml', False, True),
+    ('/hello/bob.toml', True, True),
+    ('/hello/bob.toml', False, False),
+]
+
 
 @pytest.fixture
 def tmpfile(tmpdir):
-    p = tmpdir.mkdir('testdir').join('testfile.txt')
+    p = tmpdir.mkdir('testdir').join('testfile.toml')
     p.write(tomlkit.dumps(toml_doc))
     return str(p)
 
@@ -87,7 +104,7 @@ def test_encoding(tmpfile):
     with Config(tmpfile, 'w', encoding='iso-8859-5') as file:
         file.data = test_data_iso_8859_5
     with pytest.raises(UnicodeDecodeError):
-        with Config(tmpfile, 'r'):
+        with Config(tmpfile, 'r') as file:
             pass
     with Config(tmpfile, 'r', encoding='utf-8', errors='replace') as file:
         assert file.data == test_data_utf_8
@@ -138,3 +155,22 @@ def test_get_nix_app_dir():
     )
     result = get_app_dir('Foo Bar')
     assert app_dir in result and 'foo-bar' in result
+
+
+@pytest.mark.get_filename
+@pytest.mark.parametrize('config_path, roaming, force_posix', get_filename_params)
+def test_get_filename(config_path, roaming, force_posix):
+    result = get_filename(config_path, roaming, force_posix)
+    if not config_path:
+        # Scenario 1
+        assert result == os.path.join(get_app_dir(stem(sys.argv[0]), roaming=roaming, force_posix=force_posix),
+                                      'conf.toml')
+    elif os.path.isdir(config_path):
+        # Scenario 2
+        assert result == os.path.join(config_path, 'conf.toml')
+    elif stem(config_path) == config_path:
+        # Scenario 3
+        assert result == os.path.join(get_app_dir(config_path, roaming=roaming, force_posix=force_posix), 'conf.toml')
+    elif os.path.basename(config_path).split('.')[1] == 'toml':
+        # Scenario 4
+        assert result == config_path
